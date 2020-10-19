2dd9cbfec386fc38803dd23a1fa12a735b649179
坎坎坷坷 <gyv12345@163.com>
2020-10-19 17:35:57 +0800

    修改部分代码

--- yt4j-ui/src/locales/index.js
+++ yt4j-ui/src/locales/index.js
@@ -4,11 +4,11 @@ import storage from 'store'
 import moment from 'moment'
 
 // default lang
-import enUS from './lang/en-US'
+import enUS from './lang/zh-CN'
 
 Vue.use(VueI18n)
 
-export const defaultLang = 'en-US'
+export const defaultLang = 'zh-CN'
 
 const messages = {
   'en-US': {
