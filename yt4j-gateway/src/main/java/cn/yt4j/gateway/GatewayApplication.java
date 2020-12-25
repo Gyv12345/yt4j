71eda81e50136d098ba63b686604a2a8563603c2
坎坎坷坷 <gyv12345@163.com>
2020-12-25 17:35:24 +0800

    改spring cloud

---
+++ yt4j-gateway/src/main/java/cn/yt4j/gateway/GatewayApplication.java
@@ -0,0 +1,14 @@
+package cn.yt4j.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gyv12
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
