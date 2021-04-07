60bd59e6cfbe7680f511efd906c4024b05b245e7
shichenyang <gyv12345@163.com>
2021-04-07 17:12:26 +0800

    refactor: 去掉无用的依赖

--- yt4j-commons/yt4j-flow/src/main/java/cn/yt4j/flow/work/FlowTool.java
+++ yt4j-commons/yt4j-flow/src/main/java/cn/yt4j/flow/work/FlowTool.java
@@ -14,10 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
--- yt4j-modules/yt4j-sys/src/main/java/cn/yt4j/sys/config/RsaConfig.java
+++ yt4j-modules/yt4j-sys/src/main/java/cn/yt4j/sys/config/RsaConfig.java
@@ -16,11 +16,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 /**
  * 非对称加密
  *
