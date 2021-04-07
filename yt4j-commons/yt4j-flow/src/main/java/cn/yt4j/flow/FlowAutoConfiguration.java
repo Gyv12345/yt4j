37d6aeb82aa09bbfaf02ceeee44058467b49d800
shichenyang <gyv12345@163.com>
2021-04-07 10:45:57 +0800

    feat: 增加流程处理

---
+++ yt4j-commons/yt4j-flow/src/main/java/cn/yt4j/flow/FlowAutoConfiguration.java
@@ -0,0 +1,10 @@
+package cn.yt4j.flow;
+
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author gyv12345@163.com
+ */
+@ComponentScan("cn.yt4j.flow")
+public class FlowAutoConfiguration {
+}
