252cfd34002d1d96f58ac2ab30012f372cc96c6f
shichenyang <gyv12345@163.com>
2022-08-15 15:41:36 +0800

    feat: ip属地

--- yt4j-gateway/src/main/java/cn/yt4j/gateway/filter/WrapperRequestGlobalFilter.java
+++ yt4j-gateway/src/main/java/cn/yt4j/gateway/filter/WrapperRequestGlobalFilter.java
@@ -1,6 +1,10 @@
 package cn.yt4j.gateway.filter;
 
+import cn.yt4j.gateway.util.WebFluxUtil;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.lionsoul.ip2region.xdb.Searcher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
@@ -21,25 +25,33 @@ import java.net.URI;
 @Slf4j
 public class WrapperRequestGlobalFilter implements GlobalFilter, Ordered {
 
-	/**
-	 * 优先级最高
-	 */
-	@Override
-	public int getOrder() {
-		return Ordered.HIGHEST_PRECEDENCE;
-	}
 
-	@Override
-	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-		ServerHttpRequest request = exchange.getRequest();
-		URI URIPath = request.getURI();
-		String path = request.getPath().value();
-		String method = request.getMethodValue();
-		HttpHeaders header = request.getHeaders();
-		log.info("***********************************请求信息**********************************");
-		log.info("请求request信息：URI = {}, path = {}，method = {}，header = {}。", URIPath, path, method, header);
-		log.info("****************************************************************************\n");
-		return chain.filter(exchange);
-	}
+    @Autowired
+    private Searcher searcher;
+
+    /**
+     * 优先级最高
+     */
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE - 1;
+    }
+
+    @SneakyThrows
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        URI URIPath = request.getURI();
+        String path = request.getPath().value();
+        String method = request.getMethodValue();
+        HttpHeaders header = request.getHeaders();
+        String ip = WebFluxUtil.getIpAddress(exchange.getRequest());
+        String region = searcher.search(ip);
+
+        log.info("***********************************请求信息**********************************");
+        log.info("请求request信息：URI = {}, path = {}，method = {}，header = {}，ip = {} ，region = {}。", URIPath, path, method, header, ip, region);
+        log.info("****************************************************************************\n");
+        return chain.filter(exchange);
+    }
 
 }
