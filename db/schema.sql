08bf365c31b8b089a69487b2a9ebbdba37e90634
shichenyang <gyv12345@163.com>
2022-11-15 11:10:37 +0800

    refactor: 调整一下

--- db/schema.sql
+++ db/schema.sql
@@ -10,4 +10,5 @@
 
 create database `yt4j` default character set utf8mb4 collate utf8mb4_unicode_ci;
 create database `yt4j_crm` default character set utf8mb4 collate utf8mb4_unicode_ci;
+create database `yt4j_nacos` default character set utf8mb4 collate utf8mb4_general_ci;
 
