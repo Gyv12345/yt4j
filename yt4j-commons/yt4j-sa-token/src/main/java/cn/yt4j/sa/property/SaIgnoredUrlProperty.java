1678ed8aa7a79a42032b5836e3b6829c8c36da96
shichenyang <gyv12345@163.com>
2023-12-11 21:39:22 +0800

    feat: oss和监控

--- yt4j-commons/yt4j-sa-token/src/main/java/cn/yt4j/sa/property/SaIgnoredUrlProperty.java
+++ yt4j-commons/yt4j-sa-token/src/main/java/cn/yt4j/sa/property/SaIgnoredUrlProperty.java
@@ -4,6 +4,7 @@ package cn.yt4j.sa.property;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -12,6 +13,7 @@ import java.util.List;
 /**
  * @author gyv12345@163.com
  */
+@Primary
 @RefreshScope
 @Component
 @Data
---
+++ yt4j-modules/yt4j-monitor/src/main/java/cn/yt4j/monitor/MonitorApplication.java
@@ -0,0 +1,15 @@
+package cn.yt4j.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 监控服务
+ * @author gyv12345@163.com
+ */
+@SpringBootApplication
+public class MonitorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorApplication.class, args);
+    }
+}
