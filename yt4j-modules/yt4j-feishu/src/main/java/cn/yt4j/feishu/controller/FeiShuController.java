6effd395a8d3f7e2152196e7ff9d7a6252f9d2e1
shichenyang <gyv12345@163.com>
2024-01-10 18:14:25 +0800

    feat: 企业IM

---
+++ yt4j-modules/yt4j-feishu/src/main/java/cn/yt4j/feishu/controller/FeiShuController.java
@@ -0,0 +1,17 @@
+package cn.yt4j.feishu.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author gyv12345@163.com
+ */
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@Tag(name = "飞书")
+public class FeiShuController {
+
+}
