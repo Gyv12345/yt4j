db26bd1b09155799378e9f64addf80a01915749e
shichenyang <gyv12345@163.com>
2025-04-23 08:47:00 +0800

    feat(modules): 添加知识库模块并更新相关配置
    - 新增 yt4j-knowledge 模块，包含应用配置、启动类、日志配置等
    - 更新 yt4j-modules/pom.xml，添加 yt4j-knowledge 子模块
    - 更新根 pom.xml，升级 Spring Boot、Spring Cloud 等依赖版本
    - 添加 Spring AI相关依赖

---
+++ yt4j-modules/yt4j-knowledge/src/main/java/cn/yt4j/knowledge/KnowledgeApplication.java
@@ -0,0 +1,13 @@
+package cn.yt4j.knowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeApplication.class, args);
+	}
+
+}
---
+++ yt4j-modules/yt4j-knowledge/src/test/java/cn/yt4j/knowledge/Yt4jKnowledgeApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yt4j.knowledge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yt4jKnowledgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
